5a8ebf87214061d8572800a50f73475250eee929
CodenameSushi <fhenrique08@gmail.com>
2022-12-07 16:10:37 -0300

    Header finalizado

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+
+    <>
+    <Header />
+    <h1>Homepage</h1>
+    </>
   )
 }
 
